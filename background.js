b021f468ecd8f4d01afcd08c7ef9840d56c15076
javidalpe <javier@undefinedlabs.com>
2019-05-20 15:14:47 +0200

    Update to 1.1

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        id: 'buscar',
-        title: 'Buscar en los boletines',
-        type: 'normal',
-        contexts: ['selection'],
-    });
+const searchFunction = function(info) {
+    // Encode user input for special characters , / ? : @ & = + $ #
+    var newURL = 'https://www.infoboe.com/buscar?q=' + encodeURIComponent(info.selectionText);
+    chrome.tabs.create({ url: newURL });
+};
 
-    chrome.contextMenus.onClicked.addListener(function(info) {
-        // Encode user input for special characters , / ? : @ & = + $ #
-        var newURL = 'https://www.infoboe.com/buscar?q=' + encodeURIComponent(info.selectionText);
-        chrome.tabs.create({ url: newURL });
-    });
+chrome.contextMenus.create({
+    id: 'buscar',
+    title: 'Buscar en los boletines',
+    type: 'normal',
+    contexts: ['selection']
 });
+
+chrome.contextMenus.onClicked.addListener(searchFunction)
